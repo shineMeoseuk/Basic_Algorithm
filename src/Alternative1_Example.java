574c8c34911b76519dc5d50b1d57446ab8c8b6cc
mangi0718 <white3097@gmail.com>
2023-02-07 22:14:20 +0900

    기초 알고리즘

---
+++ src/Alternative1_Example.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Alternative1_Example {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int n;
+
+        System.out.println("+와 -를 번갈아 n개 출력합니다.");
+
+        do {
+            System.out.print("n의 값: ");
+            n = stdIn.nextInt();
+        } while(n <= 0);
+
+        for(int i = 0; i < n/2; i++)
+            System.out.print("+-");
+        if (n % 2 != 0)
+            System.out.print("+");
+    }
+}
---
+++ src/SumVerbose1_Example.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class SumVerbose1_Example {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int n;
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+
+        do {
+            System.out.print("n의 값: ");
+            n = stdIn.nextInt();
+        } while(n <= 0);
+
+        int sum = 0;
+
+        for (int i=1; i<n; i++){
+            System.out.print(i + " + ");
+            sum+=i;
+        }
+
+        System.out.println(n + " = " + (sum += n));
+    }
+}
