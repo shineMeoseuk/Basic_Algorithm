82700bc56a87c25e90f2c806c0409e482609c6e4
mangi0718 <white3097@gmail.com>
2023-08-29 23:59:00 +0900

    중복 문자 제거

---
+++ src/DuplicationStr.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class DuplicationStr {
+    public String solution(String str) {
+        String answer="";
+        for(int i=0; i<str.length(); i++) {
+            //System.out.println(str.charAt(i)+" " +i+" "+str.indexOf(str.charAt(i)));
+            if(str.indexOf(str.charAt(i))==i) answer+=str.charAt(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DuplicationStr duplicationStr = new DuplicationStr();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.print(duplicationStr.solution(str));
+    }
+}
