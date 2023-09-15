ad9529efdd9255e173dd9c4ce109289f7e7854e9
Shine Meoseuk <112597600+shineMeoseuk@users.noreply.github.com>
2023-09-15 17:53:36 +0900

    가위바위보

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class RockPaperScissors {
+import java.util.Scanner;
+
+class RockPaperScissors {
+    public String solution(int n, int[] a, int[] b){
+        String answer="";
+        for(int i=0; i<n; i++){
+            if(a[i]==b[i]) answer+="D";
+            else if(a[i]==1 && b[i]==3) answer+="A";
+            else if(a[i]==2 && b[i]==1) answer+="A";
+            else if(a[i]==3 && b[i]==2) answer+="A";
+            else answer+="B";
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RockPaperScissors RPS = new RockPaperScissors();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] a=new int[n];
+        int[] b=new int[n];
+        for(int i=0; i<n; i++){
+            a[i]=kb.nextInt();
+        }
+        for(int i=0; i<n; i++){
+            b[i]=kb.nextInt();
+        }
+        for(char x : RPS.solution(n, a, b).toCharArray()) System.out.println(x);
+    }
+
 }
